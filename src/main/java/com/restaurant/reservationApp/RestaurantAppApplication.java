741a4adf84d0fa8958483392fea66b6cc4397035
ACAYIRAY <aysegulcayiraydar@gmail.com>
2021-08-24 11:45:31 +0200

    calculated daily guest in resevation package is tested

--- src/main/java/com/restaurant/reservationApp/RestaurantAppApplication.java
+++ src/main/java/com/restaurant/reservationApp/RestaurantAppApplication.java
@@ -1,15 +1,40 @@
 package com.restaurant.reservationApp;
 
+//import com.mongodb.client.MongoClient;
+//import com.mongodb.client.MongoClients;
+//import com.mongodb.client.MongoDatabase;
+import com.restaurant.reservationApp.employee.Employee;
+import com.restaurant.reservationApp.employee.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class RestaurantAppApplication {
 
+
+public class RestaurantAppApplication implements CommandLineRunner {
+
+@Autowired
+	EmployeeService employeeService;
 	public static void main(String[] args) {
 
 		SpringApplication.run(RestaurantAppApplication.class, args);
+//		try {
+//			MongoClient mongoClient = MongoClients.create(
+//					"mongodb+srv://Aysegul:1234@cluster0.xxspu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+//			MongoDatabase database = mongoClient.getDatabase("test");
+//			System.out.println("connection is ready");
+//
+//		}catch (Error e){}
+
 	}
 
 
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = new Employee("aaaa","Ibrahim","a","12345");
+		this.employeeService.createEmployee(employee);
+	}
 }
--- src/test/java/com/restaurant/reservationApp/RestaurantAppApplicationTests.java
+++ src/test/java/com/restaurant/reservationApp/RestaurantAppApplicationTests.java
@@ -1,13 +1,47 @@
 package com.restaurant.reservationApp;
 
+import com.restaurant.reservationApp.reservation.Reservation;
+//import com.restaurant.reservationApp.stubs.StubReservationService;
+import com.restaurant.reservationApp.reservation.ReservationController;
+import com.restaurant.reservationApp.reservation.ReservationService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import java.util.Map;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
 class RestaurantAppApplicationTests {
 
+	@Mock
+	ReservationService mockedService;
+
+	@InjectMocks
+	ReservationController reservationController;
+
 	@Test
-	void contextLoads() {
+	public void getAmountOfGuest() {
+
+		//Reservation reservation = new Reservation();
+		when(mockedService.getAmountOfGuest()).thenReturn(Map.of("Mustafa",100L));
+		ResponseEntity<Map<String, Long>> value = reservationController.getAmountOfGuestSoFar();
+		Assertions.assertEquals(new ResponseEntity<>(Map.of("Mustafa",100L), HttpStatus.OK), value);
+//		StubReservationService stubReservationService = new StubReservationService();
+//		Map<String, Long> amountOfGuest = stubReservationService.getAmountOfGuest();
+//		Assertions.assertEquals(Map.of("Mustafa",100L),amountOfGuest);
+
+
 	}
 
 }
